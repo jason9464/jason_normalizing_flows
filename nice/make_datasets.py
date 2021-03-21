a96b985c25341e83543bbc61a116f4af2e45d022
jason9464 <jsheo12304@postech.ac.kr>
2021-03-21 15:43:08 +0900

    add make_datasets.py

---
+++ nice/make_datasets.py
@@ -0,0 +1,36 @@
+"""
+Todo
+1. Dequantize 하기
+2. TFD dataset 찾기
+"""
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import torch.utils.data as data
+
+# MNIST datasets has error now 
+"""torchvision.datasets.MNIST.resources = [
+            ('https://ossci-datasets.s3.amazonaws.com/mnist/train-images-idx3-ubyte.gz', 'f68b3c2dcbeaaa9fbdd348bbdeb94873'),
+            ('https://ossci-datasets.s3.amazonaws.com/mnist/train-labels-idx1-ubyte.gz', 'd53e105ee54ea40749a09fcbcd1e9432'),
+            ('https://ossci-datasets.s3.amazonaws.com/mnist/t10k-images-idx3-ubyte.gz', '9fb629c4189551a2d022fa330f9573f3'),
+            ('https://ossci-datasets.s3.amazonaws.com/mnist/t10k-labels-idx1-ubyte.gz', 'ec29112dd5afa0611ce80d1b7f02629c')
+        ]"""
+
+mnist_transform = torchvision.transforms.Compose([
+      torchvision.transforms.ToTensor(),
+      torchvision.transforms.Lambda(lambda x : x.view([-1]))
+])
+mnist_trainset = torchvision.datasets.MNIST(root="./datasets/mnist", train=True,\
+     transform=mnist_transform, download=True)
+mnist_trainset_loader = data.DataLoader(dataset=mnist_trainset, shuffle=True)
+
+mnist_testset = torchvision.datasets.MNIST(root="./datasets/mnist", train=False,\
+     transform=mnist_transform, download=True)
+
+#tfd_dataset
+
+svhn_trainset = torchvision.datasets.SVHN(root="./datasets/svhn", split="train",\
+      transform=transforms.ToTensor(), download=True)
+
+cifar10_trainset = torchvision.datasets.CIFAR10(root="./datasets/cifar10", train=True,\
+      download=True, transform=transforms.ToTensor())
